c7df64ee562d0227b6855f62f3356537e110b6c9
Aleksander <Zakharov.owl@gmail.com>
2020-03-18 22:24:33 +0300

    feat: add carbon task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,17 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  let result = 0;
+  let a = 0.693/HALF_LIFE_PERIOD;
+  if (typeof (sampleActivity) !== 'string' ||
+    sampleActivity > MODERN_ACTIVITY ||
+    sampleActivity <= 0 || 
+    /^[A-Za-z]/.test(sampleActivity)
+    ) {
+    return false;
+  } else {
+    result = Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / a);
+    return result;
+  }
 };
